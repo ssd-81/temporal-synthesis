bf86da770771da2e0050df12e955160c6be9e995
ssd-81 <masterarc@yahoo.com>
2025-09-29 22:59:50 +0530

    initialized project repository and setup struct for getting problem data

--- sagar/mini-miner/README.md
+++ sagar/mini-miner/README.md
@@ -1,1 +1,10 @@
-# 
+# Mini-Miner
+
+## Problem Statement 
+This project is a simplified mining essentially. The core idea to get the fundamental process going underneath various blockchains to validate blocks (especially) proof of work blockchains like bitcoin and initially Ethereum. 
+---
+The problem to be solved is available at 
+`GET https://hackattic.com/challenges/mini_miner/problem?access_token=aaa699dde38ea86a`
+
+Submission is made to
+`POST https://hackattic.com/challenges/mini_miner/solve?access_token=aaa699dde38ea86a`
--- sagar/mini-miner/main.go
+++ sagar/mini-miner/main.go
@@ -1,1 +1,36 @@
 package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+type blockDetails struct {
+	Data [][]string `json:"data"`
+}
+
+type problemStat struct {
+	Difficulty uint8        `json:"difficulty"`
+	Block      blockDetails `json:"block"`
+}
+
+func main() {
+
+	// getting the problem statement
+	// from : https://hackattic.com/challenges/mini_miner/problem?access_token=aaa699dde38ea86a
+
+	url := "https://hackattic.com/challenges/mini_miner/problem?access_token=aaa699dde38ea86a"
+	resp, err := http.Get(url)
+	if err != nil {
+		fmt.Printf("error while getting response from the url: ", url)
+	}
+	defer resp.Body.Close()
+	// fmt.Print(resp)
+
+	// initializing the target struct
+	var blockData blockDetails
+	decoder := json.NewDecoder(resp.Body)
+	err = decoder.Decode(&blockData)
+	fmt.Print(blockData)
+}
