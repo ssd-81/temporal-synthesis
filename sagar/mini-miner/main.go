7bad70edff36feb38b052affca5820bc8425df21
ssd-81 <masterarc@yahoo.com>
2025-10-02 12:40:56 +0530

    restructured get payload struct and added post payload struct for sending nonce for verification

--- sagar/mini-miner/main.go
+++ sagar/mini-miner/main.go
@@ -1,18 +1,25 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
 	"net/http"
 )
 
 type blockDetails struct {
-	Data [][]string `json:"data"`
+	Nonce interface{}     `json:"nonce"`
+	Data  [][]interface{} `json:"data"`
 }
 
 type problemStat struct {
 	Difficulty uint8        `json:"difficulty"`
 	Block      blockDetails `json:"block"`
+}
+
+// struct for sending json payload with POST
+type Post struct {
+	Nonce string `json:"nonce"`
 }
 
 func main() {
@@ -32,5 +39,38 @@ func main() {
 	var problem problemStat
 	decoder := json.NewDecoder(resp.Body)
 	err = decoder.Decode(&problem)
-	fmt.Print(problem)
+	if err != nil {
+		fmt.Println("error while decoding the response data: ", err)
+	}
+	// fmt.Print(problem)
+
+	// sending test post request
+	postData := Post{
+		Nonce: "123232223",
+	}
+	postUrl := "https://hackattic.com/challenges/mini_miner/solve?access_token=aaa699dde38ea86a"
+	jsonPayload, err := json.Marshal(postData)
+	if err != nil {
+		fmt.Println("Error marshalling JSON:", err)
+		return
+	}
+	reqBody := bytes.NewBuffer(jsonPayload)
+	req, err := http.NewRequest("POST", postUrl, reqBody)
+	if err != nil {
+		fmt.Println("Error creating request: ", err)
+		return
+	}
+	// setting the Header for letting the server know the format of data
+	req.Header.Set("Content-Type", "application/json")
+
+	client := &http.Client{}
+	resp, err = client.Do(req)
+	if err != nil {
+		fmt.Println("Error while making POST request: ", err)
+		return
+	}
+	defer resp.Body.Close()
+	fmt.Println(resp)
+	fmt.Println("successly sent post request")
+
 }
