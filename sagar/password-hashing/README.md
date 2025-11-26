d9aae5a74ab384a8655696a1f5c4890747d37ebf
ssd-81 <arcsagar.91@gmail.com>
2025-11-26 23:49:19 +0530

    initialized structs for parsing problem statement and made a few notes

--- sagar/password-hashing/README.md
+++ sagar/password-hashing/README.md
@@ -1,1 +1,8 @@
 # password hashing 
+
+## the core idea  
+- a password, a salt and some algorithmic specific parameters will be provided 
+- calculate the sha256, hmac-sha256 , pbkdf-sha256, scrypt
+- the core idea to understand is understand how each step is progressively used to build the next one. 
+- the problem statement also says something about "secret step"; but not sure what it is ; just keep in mind 
+
--- sagar/password-hashing/main.go
+++ sagar/password-hashing/main.go
@@ -1,1 +1,29 @@
 package main
+
+// password: the password you'll operate on
+// salt: the salt we'll use - also user as a secret where necessary; keep in mind it comes base64 encoded - decode for the raw bytes
+// pbkdf2:
+// hash: the digest to use
+// rounds: the number of rounds to use
+// scrypt:
+// N: the N parameter for scrypt's KDF
+// p: the parallelization parameter
+// r: the blocksize parameter
+// buflen: intended output length in octets
+// _control: example scrypt calculated for password="rosebud", salt="pepper", N=128, p=8, n=4
+type problemStatement struct {
+	Password string `json:"password"`
+	Salt []byte `json:"salt"`
+
+}
+type pb struct {
+	Hash string `json:"hash"`
+	Rounds string `json:"rounds"`
+}
+type sc struct {
+	N string `json:"N"`
+	p string `json:"p"`
+	r string `json:"r"`
+	BufLen int `json:"buflen"` // intended output length in octets
+	Control string `json:"_control"` // not sure what this is 
+}
