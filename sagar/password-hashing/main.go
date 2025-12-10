e871b36ef08d7d309958b686a14987edd9d490a2
ssd-81 <arcsagar.91@gmail.com>
2025-12-11 01:24:56 +0530

    made changes to input structs to properly unmarshal json

--- sagar/password-hashing/main.go
+++ sagar/password-hashing/main.go
@@ -1,5 +1,13 @@
 package main
 
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+)
+
 // password: the password you'll operate on
 // salt: the salt we'll use - also user as a secret where necessary; keep in mind it comes base64 encoded - decode for the raw bytes
 // pbkdf2:
@@ -13,17 +21,35 @@ package main
 // _control: example scrypt calculated for password="rosebud", salt="pepper", N=128, p=8, n=4
 type problemStatement struct {
 	Password string `json:"password"`
-	Salt []byte `json:"salt"`
+	Salt     string `json:"salt"` // base64 encoded
+	Pb pb `json:"pbkdf2"`
+	Sc sc `json:"scrypt"`
 
 }
 type pb struct {
-	Hash string `json:"hash"`
-	Rounds string `json:"rounds"`
+	Hash   string `json:"hash"`
+	Rounds int32 `json:"rounds"`
+	
 }
 type sc struct {
-	N string `json:"N"`
-	p string `json:"p"`
-	r string `json:"r"`
-	BufLen int `json:"buflen"` // intended output length in octets
-	Control string `json:"_control"` // not sure what this is 
+	N       int32 `json:"N"`
+	p       int32 `json:"p"`
+	r       int32 `json:"r"`
+	BufLen  int32    `json:"buflen"`   // intended output length in octets
+	Control int32 `json:"_control"` // not sure what this is
+}
+
+// get: https://hackattic.com/challenges/password_hashing/problem?access_token=aaa699dde38ea86a
+
+func main() {
+
+	resp, err := http.Get("https://hackattic.com/challenges/password_hashing/problem?access_token=aaa699dde38ea86a")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer resp.Body.Close()
+	body, _ := io.ReadAll(resp.Body)
+	var problem problemStatement
+	json.Unmarshal(body, &problem)
+	fmt.Print(problem)
 }
