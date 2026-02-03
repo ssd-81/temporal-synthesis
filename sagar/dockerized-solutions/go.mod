9ec0cf493ef6021aa164a3c27fca804f8c03219d
ssd-81 <arcsagar.91@gmail.com>
2026-02-04 00:50:31 +0530

    setting up the flow for processing the problem and plumbing

---
+++ sagar/dockerized-solutions/go.mod
@@ -0,0 +1,3 @@
+module github.com/ssd-81/temporal-synthesis/sagar/dockerized-solutions
+
+go 1.25.4
---
+++ sagar/dockerized-solutions/main.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+)
+
+type problemStat struct {
+	Credentials struct {
+		User     string `json:"user"`
+		Password string `json:"password"`
+	} `json:"credentials"`
+	IgnitionKey  string `json:"ignition_key"`
+	TriggerToken string `json:"trigger_token"`
+}
+
+type TriggerPost struct {
+	RegistryHost string `json:"registry_host"`
+}
+
+type SolutionPost struct {
+	Secret string `json:"secret"`
+}
+
+func main() {
+
+	url := "https://hackattic.com/challenges/dockerized_solutions/problem?access_token=aaa699dde38ea86a"
+	resp, err := http.Get(url)
+	if err != nil {
+		fmt.Printf("error: %v", err)
+	}
+	defer resp.Body.Close()
+
+	var problem problemStat
+	decoder := json.NewDecoder(resp.Body)
+	err = decoder.Decode(&problem)
+	if err != nil {
+		fmt.Println("error encountered while decoding input problem")
+	}
+
+
+
+	// posting json for trigger
+	triggerUrl := "https://hackattic.com/_/push/" + problem.TriggerToken
+	triggerPayload := TriggerPost{
+		RegistryHost: "fbi.gov",
+	}
+
+	jsonTrigger, _ := json.Marshal(triggerPayload)
+	resp, err = http.Post(triggerUrl, "application/json", bytes.NewBuffer(jsonTrigger))
+	if err != nil {
+		return
+	}
+	defer resp.Body.Close()
+
+	body, _ := io.ReadAll(resp.Body)
+	fmt.Println(string(body))
+
+
+	submitUrl := "https://hackattic.com/challenges/dockerized_solutions/solve?access_token=aaa699dde38ea86a"
+	solution := SolutionPost{Secret: "test"}
+	jsonPayload, _ := json.Marshal(solution)
+
+	resp, err = http.Post(submitUrl, "application/json", bytes.NewBuffer(jsonPayload))
+	if err != nil {
+		return
+	}
+	defer resp.Body.Close()
+
+	finalBody, _ := io.ReadAll(resp.Body)
+	fmt.Println(string(finalBody))
+}
