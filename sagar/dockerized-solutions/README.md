e823459ba6ec93654fb998b6d360f01ad3711d24
ssd-81 <arcsagar.91@gmail.com>
2026-03-05 23:50:11 +0530

    problem: credential that are being used to setup the docker server might need to be dynamically created; because a new problem statement is provided in each run

--- sagar/dockerized-solutions/README.md
+++ sagar/dockerized-solutions/README.md
@@ -1,1 +1,5 @@
 ## Understanding the problem we are trying to solve
+
+
+### docker host
+https://hackattic-registry-sagar-hackattic-docker-damp-feather-1085.fly.dev/
--- sagar/dockerized-solutions/main.go
+++ sagar/dockerized-solutions/main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
+	"io"
 	"net/http"
 )
 
@@ -38,25 +40,28 @@ func main() {
 	if err != nil {
 		fmt.Println("error encountered while decoding input problem")
 	}
-	fmt.Println(problem.Credentials.User, problem.Credentials.Password)
+	
+	// pretty, _ := json.MarshalIndent(problem, "", "  ")
+	// fmt.Println(string(pretty))
 
 
+	trToken := "b3a23158.a53d.44a4.aa62.f324a91dbc60"
 
 	// posting json for trigger
-	// triggerUrl := "https://hackattic.com/_/push/" + problem.TriggerToken
-	// triggerPayload := TriggerPost{
-	// 	RegistryHost: "fbi.gov",
-	// }
+	triggerUrl := "https://hackattic.com/_/push/" + trToken
+	triggerPayload := TriggerPost{
+		RegistryHost: "https://hackattic-registry-sagar-hackattic-docker-cold-glitter-665.fly.dev/",
+	}
 
-	// jsonTrigger, _ := json.Marshal(triggerPayload)
-	// resp, err = http.Post(triggerUrl, "application/json", bytes.NewBuffer(jsonTrigger))
-	// if err != nil {
-	// 	return
-	// }
-	// defer resp.Body.Close()
+	jsonTrigger, _ := json.Marshal(triggerPayload)
+	resp, err = http.Post(triggerUrl, "application/json", bytes.NewBuffer(jsonTrigger))
+	if err != nil {
+		return
+	}
+	defer resp.Body.Close()
 
-	// body, _ := io.ReadAll(resp.Body)
-	// fmt.Println(string(body))
+	body, _ := io.ReadAll(resp.Body)
+	fmt.Println(string(body))
 
 
 	// submitUrl := "https://hackattic.com/challenges/dockerized_solutions/solve?access_token=aaa699dde38ea86a"
