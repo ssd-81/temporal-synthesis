916c6f90b54dbccf39072290b5d5ee8d529bc104
ssd-81 <arcsagar.91@gmail.com>
2026-04-11 00:28:26 +0530

    pending: trigger token gets invalidated; so caching is not possible

--- sagar/dockerized-solutions/main.go
+++ sagar/dockerized-solutions/main.go
@@ -6,7 +6,6 @@ import (
 	"fmt"
 	"io"
 	"net/http"
-	"os"
 )
 
 type problemStat struct {
@@ -26,54 +25,44 @@ type SolutionPost struct {
 	Secret string `json:"secret"`
 }
 
-const credsFile = "credentials.json"
+// testing for flow
+// caching the problem is invalidates it
+const (
+	accessToken  = "aaa699dde38ea86a"
+	registryHost = "hackattic-registry-sagar-hackattic-docker-damp-glade-7985.fly.dev"
+)
 
 func main() {
 	var problem problemStat
-	url := "https://hackattic.com/challenges/dockerized_solutions/problem?access_token=aaa699dde38ea86a"
-
-	if data, err := os.ReadFile(credsFile); err == nil {
-		json.Unmarshal(data, &problem)
-	} else {
-		resp, err := http.Get(url)
-		if err != nil {
-			fmt.Println("error: Get url")
-			return
-		}
-		defer resp.Body.Close()
-
-		body, err := io.ReadAll(resp.Body)
-		if err != nil {
-			fmt.Println("error: reading response body")
-			return
-		}
-
-		if err := json.Unmarshal(body, &problem); err != nil {
-			fmt.Println("error: unmarshaling problem")
-			return
-		}
-
-		os.WriteFile(credsFile, body, 0644)
+	// not caching the problem
+	url := "https://hackattic.com/challenges/dockerized_solutions/problem?access_token=" + accessToken
+	resp, err := http.Get(url)
+	if err != nil {
+		fmt.Println("error fetching problem:", err)
+		return
 	}
+	defer resp.Body.Close()
+	body, _ := io.ReadAll(resp.Body)
 
-	pretty, _ := json.MarshalIndent(problem, "", "  ")
-	fmt.Println(string(pretty))
-
-	fmt.Println("waiting for manual execution: ")
-	fmt.Scanln()
+	if err := json.Unmarshal(body, &problem); err != nil {
+		fmt.Println("error unmarshaling:", err)
+		return
+	}
+	prettyProblem, _ := json.MarshalIndent(problem, "", "  ")
+	fmt.Println(string(prettyProblem))
 
 	triggerUrl := "https://hackattic.com/_/push/" + problem.TriggerToken
 	triggerPayload := TriggerPost{
-		RegistryHost: "hackattic-registry-sagar-hackattic-docker-cold-glitter-665.fly.dev",
+		RegistryHost: "hackattic-registry-sagar-hackattic-docker-damp-glade-7985.fly.dev",
 	}
 
 	jsonTrigger, _ := json.Marshal(triggerPayload)
-	resp, err := http.Post(triggerUrl, "application/json", bytes.NewBuffer(jsonTrigger))
+	resp, err = http.Post(triggerUrl, "application/json", bytes.NewBuffer(jsonTrigger))
 	if err != nil {
 		return
 	}
 	defer resp.Body.Close()
 
-	body, _ := io.ReadAll(resp.Body)
+	body, _ = io.ReadAll(resp.Body)
 	fmt.Println(string(body))
 }
